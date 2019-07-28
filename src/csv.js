503850aa47157d80d953b7df23a67bc6fff4446e
Evan Plaice <evanplaice@gmail.com>
2019-07-27 21:16:38 -0600

    Start mocking the API

---
+++ src/csv.js
@@ -0,0 +1,20 @@
+
+const CSV;
+
+CSV.parse = (input, hook, objects) => {
+  // TODO: Add input checking
+
+  // TODO: Array-specific stuff
+
+  // TODO: Object-specific stuff
+}
+
+CSV.stringify = (input, hook, objects) => {
+  // TODO: Add input checking
+
+  // TODO: Array-specific stuff
+
+  // TODO: Object-specific stuff
+}
+
+export default CSV
