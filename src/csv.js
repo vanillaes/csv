bc5eb857462d33c110130ecd0ffd10b69430f922
Evan Plaice <evanplaice@gmail.com>
2019-11-20 23:09:12 -0700

    Update param defaults

--- src/csv.js
+++ src/csv.js
@@ -1,11 +1,11 @@
 export default class CSV {
-  static parse (input, options = {}, reviver) {
+  static parse (input, options = {}, reviver = v => v) {
     // TODO: Add input checking
 
     // TODO: Add parser
   }
 
-  static stringify (input, options = {}, replacer) {
+  static stringify (input, options = {}, replacer = null) {
     // TODO: Add input checking
 
     // TODO: Add formatter
