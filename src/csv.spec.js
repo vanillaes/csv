890b8ae0da75b0074d74f7dcb780f02f0e87db84
Evan Plaice <evanplaice@gmail.com>
2019-11-20 23:06:15 -0700

    Scaffold the tests

---
+++ src/csv.spec.js
@@ -0,0 +1,72 @@
+import  test from 'tape';
+import csv from './csv.js';
+
+// TODO: Implement this
+test('RFC Rule #1 - One entry per line, each line ends with a newline', (t) => {
+  const expect = null;
+  const result = null;
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
+
+// TODO: Implement this
+test('RFC Rule #2 - Trailing newline at the end of the file ommitted', (t) => {
+  const expect = null;
+  const result = null;
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
+
+// TODO: Implement this
+test('RFC Rule #3 - First row contains header data', (t) => {
+  const expect = null;
+  const result = null;
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
+
+// TODO: Implement this
+test('RFC Rule #4 - Spaces are considered data and entries should not contain a trailing comma', (t) => {
+  const expect = null;
+  const result = null;
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
+
+// TODO: Implement this
+test('RFC Rule #5 - Lines may or may not be delimited by double-quotes', (t) => {
+  const expect = null;
+  const result = null;
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
+
+// TODO: Implement this
+test('RFC Rule #6 - Fields containing line breaks, double-quotes, and commas should be enclosed in double-quotes', (t) => {
+  const expect = null;
+  const result = null;
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
+
+// TODO: Implement this
+test('RFC Rule #7 - If double-quotes are used to enclose fields, then a double-quote appering inside a field must be escaped by a preceding it with another double-quote', (t) => {
+  const expect = null;
+  const result = null;
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
