a89b26565b44dd1fb25a0256e441aed35a6ab46e
Evan Plaice <evanplaice@gmail.com>
2019-12-04 13:40:13 -0700

    CSV.stringify() RFC 1 Test Pass

---
+++ src/stringify.rfc.spec.js
@@ -0,0 +1,74 @@
+import test from 'tape';
+import CSV from './csv.js';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const rfc1 = require('./__test__/rfc1.json');
+const rfc2 = require('./__test__/rfc2.json');
+const rfc3 = require('./__test__/rfc3.json');
+const rfc4 = require('./__test__/rfc4.json');
+const rfc5 = require('./__test__/rfc5.json');
+const rfc6 = require('./__test__/rfc6.json');
+const rfc7 = require('./__test__/rfc7.json');
+
+test('RFC Rule #1 - One entry per line, each line ends with a newline', (t) => {
+  const expect = rfc1.csv.join('\n');
+  const result = CSV.stringify(rfc1.json);
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
+
+// test('RFC Rule #2 - Trailing newline at the end of the file ommitted', (t) => {
+//   const expect = rfc2.json;
+//   const result = CSV.parse(rfc2.csv.join('\n'));
+
+//   t.deepEqual(result, expect);
+
+//   t.end();
+// });
+
+// test('RFC Rule #3 - First row contains header data', (t) => {
+//   const expect = rfc3.json;
+//   const result = CSV.parse(rfc3.csv.join('\n'));
+
+//   t.deepEqual(result, expect);
+
+//   t.end();
+// });
+
+// test('RFC Rule #4 - Spaces are considered data and entries should not contain a trailing comma', (t) => {
+//   const expect = rfc4.json;
+//   const result = CSV.parse(rfc4.csv.join('\n'));
+
+//   t.deepEqual(result, expect);
+
+//   t.end();
+// });
+
+// test('RFC Rule #5 - Lines may or may not be delimited by double-quotes', (t) => {
+//   const expect = rfc5.json;
+//   const result = CSV.parse(rfc5.csv.join('\n'));
+
+//   t.deepEqual(result, expect);
+
+//   t.end();
+// });
+
+// test('RFC Rule #6 - Fields containing line breaks, double-quotes, and commas should be enclosed in double-quotes', (t) => {
+//   const expect = rfc6.json;
+//   const result = CSV.parse(rfc6.csv.join('\n'));
+
+//   t.deepEqual(result, expect);
+
+//   t.end();
+// });
+
+// test('RFC Rule #7 - If double-quotes are used to enclose fields, then a double-quote appering inside a field must be escaped by a preceding it with another double-quote', (t) => {
+//   const expect = rfc7.json;
+//   const result = CSV.parse(rfc7.csv.join('\n'));
+
+//   t.deepEqual(result, expect);
+
+//   t.end();
+// });
