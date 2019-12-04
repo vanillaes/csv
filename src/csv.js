707515450999004c596e81ea879c856216bbf0f9
Evan Plaice <evanplaice@gmail.com>
2019-12-04 00:58:00 -0700

    CSV.parse() RFC 1 Test Pass

--- src/csv.js
+++ src/csv.js
@@ -1,8 +1,58 @@
 export default class CSV {
   static parse (input, options = {}, reviver = v => v) {
     // TODO: Add input checking
+    let match = '';
+    let state = 0;
+    const ctx = Object.create(null);
+    ctx.value = '';
+    ctx.entry = [];
+    ctx.output = [];
+    ctx.col = 1;
+    ctx.row = 1;
 
-    // TODO: Add parser
+    const lexer = RegExp(/"|,|\r\n|\n|\r|[^",\r\n]+/y);
+
+    while(match = lexer.exec(input)) {
+      match = match[0];
+
+      switch (state) {
+        case 0: // start of entry
+          switch(true) {
+            case match === '"':
+              state = 1;
+              break;
+            case match === ',':
+              valueEnd(ctx);
+              break;
+            case /^(\r\n|\n|\r)$/.test(match):
+              valueEnd(ctx);
+              entryEnd(ctx);
+              break;
+            default:
+              ctx.value += match;
+              state = 3;
+              break;
+          }
+          break;
+        case 3: // un-delimited input
+          switch(true) {
+            case match === ',':
+              state = 0;
+              this.valueEnd(ctx);
+              break;
+            case /^(\r\n|\n|\r)$/.test(match):
+              state = 0;
+              this.valueEnd(ctx);
+              this.entryEnd(ctx);
+              break;
+            default:
+              state = 4;
+              throw Error('CSVError: Illegal state [row:' + ctx.row + ']');
+          }
+          break;
+      }
+    }
+    return ctx.output;
   }
 
   static stringify (input, options = {}, replacer = null) {
@@ -10,4 +60,20 @@ export default class CSV {
 
     // TODO: Add formatter
   }
+
+  static valueEnd (ctx) {
+    ctx.entry.push(ctx.value);
+    
+    ctx.value = '';
+    ctx.col++;
+  }
+
+  static entryEnd (ctx) {
+    ctx.output.push(ctx.entry);
+
+    ctx.entry = [];
+    ctx.row++;
+    ctx.col = 1;
+  }
+
 }
