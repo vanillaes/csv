628694630e24fb0ed38ecbcd2047a48a00ab7fc4
Evan Plaice <evanplaice@gmail.com>
2020-01-20 17:01:14 -0700

    Add Reviver Tests/Fixtures

--- src/options.spec.js
+++ src/options.spec.js
@@ -2,11 +2,21 @@ import test from 'tape';
 import CSV from './csv.js';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const reviver = require('./__test__/reviver.json');
+const reviver1 = require('./__test__/reviver1.json');
+const reviver2 = require('./__test__/reviver2.json');
 
-test('RFC Rule #1 - One entry per line, each line ends with a newline', (t) => {
-  const expect = reviver.json;
-  const result = CSV.parse(reviver.csv.join('\n'), null, (value) => value + '1');
+test('Reviver #1 - The reviver should append 1 to each value', (t) => {
+  const expect = reviver1.json;
+  const result = CSV.parse(reviver1.csv.join('\n'), null, (value) => value + '1');
+
+  t.deepEqual(result, expect);
+
+  t.end();
+});
+
+test('Reviver #2 - The reviver should output the row:col values', (t) => {
+  const expect = reviver2.json;
+  const result = CSV.parse(reviver2.csv.join('\n'), null, (value, row, col) => `${row}:${col}`);
 
   t.deepEqual(result, expect);
 
