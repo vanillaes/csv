d45c9e2b56490d981a92d006db5ae467b9e95bc3
Evan Plaice <evanplaice@gmail.com>
2020-02-06 17:08:27 -0700

    Make Tree-Shake Friendly
    - convert the util class to functions
    - remove re-exports
    - move src/csv.js -> index.js
    - rebuild source

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,31 @@
-export { default } from "./src/csv.js";
+/**
+ * Parse takes a string of CSV data and converts it to a 2 dimensional array
+ *
+ * options
+ * - typed - infer types [false]
+ *
+ * @static
+ * @param {string} csv the CSV string to parse
+ * @param {Object} [options] an object containing the options
+ * @param {Function} [reviver] a custom function to modify the values
+ * @returns {Array} a 2 dimensional array of `[entries][values]`
+ */
+export function parse(csv: string, options?: any, reviver?: Function): any[];
+/**
+ * Stringify takes a 2 dimensional array of `[entries][values]` and converts them to CSV
+ *
+ * options
+ * - eof - add a trailing newline at the end of file [true]
+ *
+ * @static
+ * @param {Array} array the input array to stringify
+ * @param {Object} [options] an object containing the options
+ * @param {Function} [replacer] a custom function to modify the values
+ * @returns {string} the CSV string
+ */
+export function stringify(array: any[], options?: any, replacer?: Function): string;
+declare namespace _default {
+    export { parse };
+    export { stringify };
+}
+export default _default;
--- index.js
+++ index.js
@@ -1,1 +1,191 @@
-export { default } from './src/csv.js';
+/**
+ * Parse takes a string of CSV data and converts it to a 2 dimensional array
+ *
+ * options
+ * - typed - infer types [false]
+ *
+ * @static
+ * @param {string} csv the CSV string to parse
+ * @param {Object} [options] an object containing the options
+ * @param {Function} [reviver] a custom function to modify the values
+ * @returns {Array} a 2 dimensional array of `[entries][values]`
+ */
+export function parse (csv, options, reviver = v => v) {
+  const ctx = Object.create(null);
+  ctx.options = options || {};
+  ctx.reviver = reviver;
+  ctx.value = '';
+  ctx.entry = [];
+  ctx.output = [];
+  ctx.col = 1;
+  ctx.row = 1;
+
+  const lexer = RegExp(/"|,|\r\n|\n|\r|[^",\r\n]+/y);
+
+  let matches = [];
+  let match = '';
+  let state = 0;
+
+  while ((matches = lexer.exec(csv)) !== null) {
+    match = matches[0];
+
+    switch (state) {
+      case 0: // start of entry
+        switch (true) {
+          case match === '"':
+            state = 3;
+            break;
+          case match === ',':
+            state = 0;
+            valueEnd(ctx);
+            break;
+          case /^(\r\n|\n|\r)$/.test(match):
+            state = 0;
+            valueEnd(ctx);
+            entryEnd(ctx);
+            break;
+          default:
+            ctx.value += match;
+            state = 2;
+            break;
+        }
+        break;
+      case 2: // un-delimited input
+        switch (true) {
+          case match === ',':
+            state = 0;
+            valueEnd(ctx);
+            break;
+          case /^(\r\n|\n|\r)$/.test(match):
+            state = 0;
+            valueEnd(ctx);
+            entryEnd(ctx);
+            break;
+          default:
+            state = 4;
+            throw Error(`CSVError: Illegal state [row:${ctx.row}, col:${ctx.col}]`);
+        }
+        break;
+      case 3: // delimited input
+        switch (true) {
+          case match === '"':
+            state = 4;
+            break;
+          default:
+            state = 3;
+            ctx.value += match;
+            break;
+        }
+        break;
+      case 4: // escaped or closing delimiter
+        switch (true) {
+          case match === '"':
+            state = 3;
+            ctx.value += match;
+            break;
+          case match === ',':
+            state = 0;
+            valueEnd(ctx);
+            break;
+          case /^(\r\n|\n|\r)$/.test(match):
+            state = 0;
+            valueEnd(ctx);
+            entryEnd(ctx);
+            break;
+          default:
+            throw Error(`CSVError: Illegal state [row:${ctx.row}, col:${ctx.col}]`);
+        }
+        break;
+    }
+  }
+
+  // flush the last value
+  if (ctx.entry.length !== 0) {
+    valueEnd(ctx);
+    entryEnd(ctx);
+  }
+
+  return ctx.output;
+}
+
+/**
+ * Stringify takes a 2 dimensional array of `[entries][values]` and converts them to CSV
+ *
+ * options
+ * - eof - add a trailing newline at the end of file [true]
+ *
+ * @static
+ * @param {Array} array the input array to stringify
+ * @param {Object} [options] an object containing the options
+ * @param {Function} [replacer] a custom function to modify the values
+ * @returns {string} the CSV string
+ */
+export function stringify (array, options = {}, replacer = v => v) {
+  const ctx = Object.create(null);
+  ctx.options = options;
+  ctx.options.eof = ctx.options.eof !== undefined ? ctx.options.eof : true;
+  ctx.row = 1;
+  ctx.col = 1;
+  ctx.output = '';
+
+  array.forEach((row, rIdx) => {
+    let entry = '';
+    ctx.col = 1;
+    row.forEach((col, cIdx) => {
+      if (typeof col === 'string') {
+        col = col.replace('"', '""');
+        col = /"|,|\r\n|\n|\r/.test(col) ? `"${col}"` : col;
+      }
+      entry += replacer(col, ctx.row, ctx.col);
+      if (cIdx !== row.length - 1) {
+        entry += ',';
+      }
+      ctx.col++;
+    });
+    switch (true) {
+      case ctx.options.eof:
+      case !ctx.options.eof && rIdx !== array.length - 1:
+        ctx.output += `${entry}\n`;
+        break;
+      default:
+        ctx.output += `${entry}`;
+        break;
+    }
+    ctx.row++;
+  });
+
+  return ctx.output;
+}
+
+/** @private */
+function valueEnd (ctx) {
+  const value = ctx.options.typed ? inferType(ctx.value) : ctx.value;
+  ctx.entry.push(ctx.reviver(value, ctx.row, ctx.col));
+  ctx.value = '';
+  ctx.col++;
+}
+
+/** @private */
+function entryEnd (ctx) {
+  ctx.output.push(ctx.entry);
+  ctx.entry = [];
+  ctx.row++;
+  ctx.col = 1;
+}
+
+/** @private */
+function inferType (value) {
+  switch (true) {
+    case value === 'true':
+    case value === 'false':
+      return value === 'true';
+    case /.\./.test(value):
+      return parseFloat(value);
+    case isFinite(value):
+      return parseInt(value);
+    default:
+      return value;
+  }
+}
+
+export default { parse, stringify };
