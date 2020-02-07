06c28c964c3dd6b96c92b7c4ef4accf05e707237
Evan Plaice <evanplaice@gmail.com>
2020-02-06 17:14:51 -0700

    Integrate the CJS Compat Test

---
+++ src/compat.spec.cjs
@@ -0,0 +1,21 @@
+const test = require('tape');
+const CSV = require('csv-es');
+const rfc1 = require('./__test__/rfc1.json');
+
+test('CSV.parse() - should be require-able via CommonJS', t => {
+  const expect = rfc1.json;
+  const actual = CSV.parse(rfc1.csv.join('\n'));
+
+  t.deepEqual(actual, expect, 'CSV.parse require');
+
+  t.end();
+});
+
+test('CSV.stringify() - should be require-able via CommonJS', t => {
+  const expect = rfc1.csv.join('\n');
+  const actual = CSV.stringify(rfc1.json);
+
+  t.deepEqual(actual, expect, 'CSV.stringify require');
+
+  t.end();
+});
