3d60b6d1c560c7c9f2a121c220c7b952d5a54e26
Evan Plaice <evanplaice@gmail.com>
2021-05-01 15:07:29 -0600

    Build/Minify with ESMTK

--- index.cjs
+++ index.cjs
@@ -1,201 +1,166 @@
-'use strict';
+var __defProp = Object.defineProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", {value: true});
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, {get: all[name], enumerable: true});
+};
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
-/**
- * Parse takes a string of CSV data and converts it to a 2 dimensional array
- *
- * options
- * - typed - infer types [false]
- *
- * @static
- * @param {string} csv the CSV string to parse
- * @param {Object} [options] an object containing the options
- * @param {Function} [reviver] a custom function to modify the values
- * @returns {Array} a 2 dimensional array of `[entries][values]`
- */
-function parse (csv, options, reviver = v => v) {
+// index.js
+__markAsModule(exports);
+__export(exports, {
+  parse: () => parse,
+  stringify: () => stringify
+});
+function parse(csv, options, reviver = (v) => v) {
   const ctx = Object.create(null);
   ctx.options = options || {};
   ctx.reviver = reviver;
-  ctx.value = '';
+  ctx.value = "";
   ctx.entry = [];
   ctx.output = [];
   ctx.col = 1;
   ctx.row = 1;
-
   const lexer = /"|,|\r\n|\n|\r|[^",\r\n]+/y;
   const isNewline = /^(\r\n|\n|\r)$/;
-
   let matches = [];
-  let match = '';
+  let match = "";
   let state = 0;
-
   while ((matches = lexer.exec(csv)) !== null) {
     match = matches[0];
-
     switch (state) {
-      case 0: // start of entry
+      case 0:
         switch (true) {
           case match === '"':
             state = 3;
-            break
-          case match === ',':
+            break;
+          case match === ",":
             state = 0;
             valueEnd(ctx);
-            break
+            break;
           case isNewline.test(match):
             state = 0;
             valueEnd(ctx);
             entryEnd(ctx);
-            break
+            break;
           default:
             ctx.value += match;
             state = 2;
-            break
+            break;
         }
-        break
-      case 2: // un-delimited input
+        break;
+      case 2:
         switch (true) {
-          case match === ',':
+          case match === ",":
             state = 0;
             valueEnd(ctx);
-            break
+            break;
           case isNewline.test(match):
             state = 0;
             valueEnd(ctx);
             entryEnd(ctx);
-            break
+            break;
           default:
             state = 4;
-            throw Error(`CSVError: Illegal state [row:${ctx.row}, col:${ctx.col}]`)
+            throw Error(`CSVError: Illegal state [row:${ctx.row}, col:${ctx.col}]`);
         }
-        break
-      case 3: // delimited input
+        break;
+      case 3:
         switch (true) {
           case match === '"':
             state = 4;
-            break
+            break;
           default:
             state = 3;
             ctx.value += match;
-            break
+            break;
         }
-        break
-      case 4: // escaped or closing delimiter
+        break;
+      case 4:
         switch (true) {
           case match === '"':
             state = 3;
             ctx.value += match;
-            break
-          case match === ',':
+            break;
+          case match === ",":
             state = 0;
             valueEnd(ctx);
-            break
+            break;
           case isNewline.test(match):
             state = 0;
             valueEnd(ctx);
             entryEnd(ctx);
-            break
+            break;
           default:
-            throw Error(`CSVError: Illegal state [row:${ctx.row}, col:${ctx.col}]`)
+            throw Error(`CSVError: Illegal state [row:${ctx.row}, col:${ctx.col}]`);
         }
-        break
+        break;
     }
   }
-
-  // flush the last value
   if (ctx.entry.length !== 0) {
     valueEnd(ctx);
     entryEnd(ctx);
   }
-
-  return ctx.output
+  return ctx.output;
 }
-
-/**
- * Stringify takes a 2 dimensional array of `[entries][values]` and converts them to CSV
- *
- * options
- * - eof - add a trailing newline at the end of file [true]
- *
- * @static
- * @param {Array} array the input array to stringify
- * @param {Object} [options] an object containing the options
- * @param {Function} [replacer] a custom function to modify the values
- * @returns {string} the CSV string
- */
-function stringify (array, options = {}, replacer = v => v) {
+function stringify(array, options = {}, replacer = (v) => v) {
   const ctx = Object.create(null);
   ctx.options = options;
-  ctx.options.eof = ctx.options.eof !== undefined ? ctx.options.eof : true;
+  ctx.options.eof = ctx.options.eof !== void 0 ? ctx.options.eof : true;
   ctx.row = 1;
   ctx.col = 1;
-  ctx.output = '';
-
+  ctx.output = "";
   const needsDelimiters = /"|,|\r\n|\n|\r/;
-
   array.forEach((row, rIdx) => {
-    let entry = '';
+    let entry = "";
     ctx.col = 1;
     row.forEach((col, cIdx) => {
-      if (typeof col === 'string') {
+      if (typeof col === "string") {
         col = col.replace('"', '""');
         col = needsDelimiters.test(col) ? `"${col}"` : col;
       }
       entry += replacer(col, ctx.row, ctx.col);
       if (cIdx !== row.length - 1) {
-        entry += ',';
+        entry += ",";
       }
       ctx.col++;
     });
     switch (true) {
       case ctx.options.eof:
-      case !ctx.options.eof && rIdx !== array.length - 1:
-        ctx.output += `${entry}\n`;
-        break
+      case (!ctx.options.eof && rIdx !== array.length - 1):
+        ctx.output += `${entry}
+`;
+        break;
       default:
         ctx.output += `${entry}`;
-        break
+        break;
     }
     ctx.row++;
   });
-
-  return ctx.output
+  return ctx.output;
 }
-
-/** @private */
-function valueEnd (ctx) {
+function valueEnd(ctx) {
   const value = ctx.options.typed ? inferType(ctx.value) : ctx.value;
   ctx.entry.push(ctx.reviver(value, ctx.row, ctx.col));
-  ctx.value = '';
+  ctx.value = "";
   ctx.col++;
 }
-
-/** @private */
-function entryEnd (ctx) {
+function entryEnd(ctx) {
   ctx.output.push(ctx.entry);
   ctx.entry = [];
   ctx.row++;
   ctx.col = 1;
 }
-
-/** @private */
-function inferType (value) {
+function inferType(value) {
   const isNumber = /.\./;
-
   switch (true) {
-    case value === 'true':
-    case value === 'false':
-      return value === 'true'
+    case value === "true":
+    case value === "false":
+      return value === "true";
     case isNumber.test(value):
-      return parseFloat(value)
+      return parseFloat(value);
     case isFinite(value):
-      return parseInt(value)
+      return parseInt(value);
     default:
-      return value
+      return value;
   }
 }
-
-exports.parse = parse;
-exports.stringify = stringify;
